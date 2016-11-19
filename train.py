1358b12c00ff5c366b00d063aad6eb1d641259e1
beamandrew <beam.andrew@gmail.com>
2016-11-19 14:47:57 -0500

    files added

--- train.py
+++ train.py
@@ -4,5 +4,9 @@ import numpy as np
 import tensorflow as tf
 from data_utils import Vocabulary, Dataset
 
-pvocab = Vocabulary.from_file("1b_word_vocab.txt")
-print("loaded vocab, num tokens " + str(pvocab._num_tokens))
+vocab = Vocabulary.from_file("1b_word_vocab.txt")
+print("loaded vocab, num tokens " + str(vocab._num_tokens))
+
+print("Loading data...")
+dataset = Dataset(vocab, "/mnt/raid1/billion-word-corpus/1-billion-word-language-modeling-benchmark/training-monolingual.tokenized.shuffled/*")
+print("Data loaded")
