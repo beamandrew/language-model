a6e5b5ddd975ff88843b9c8200d3a1a42c8e513c
beamandrew <beam.andrew@gmail.com>
2016-11-27 12:33:32 -0500

    basic functionality implemented

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from keras.models import Model
 from keras.layers import Dense, Dropout, Embedding, LSTM, Input, TimeDistributed, Activation
 from keras.optimizers import Adam,SGD
 import keras.backend as K
-
+import numpy as np
 
 class LanguageModel(object):
     def __init__(self,params):
@@ -12,11 +12,13 @@ class LanguageModel(object):
         self.seq_len = params['seq_len']
         self.vocab_size = params['vocab_size']
         self.hidden_dim = params['hidden_dim']
+        self.num_layers = params['num_layers']
         # Set up the input placeholder
         self.input_seq = tf.placeholder(tf.float32, shape=(self.batch_size, self.seq_len))
         # Build the RNN
         self.rnn = Embedding(self.vocab_size + 1, 128, input_length=self.seq_len)(self.input_seq)
-        self.rnn = LSTM(output_dim=self.hidden_dim, return_sequences=True, name='rnn_1')(self.rnn)
+        for l in range(self.num_layers):
+            self.rnn = LSTM(output_dim=self.hidden_dim, return_sequences=True, name='rnn_1')(self.rnn)
         rnn_output = tf.unpack(self.rnn, axis=1)
         self.w_proj = tf.Variable(tf.zeros([self.vocab_size, self.hidden_dim]))
         self.b_proj = tf.Variable(tf.zeros([self.vocab_size]))
@@ -27,11 +29,12 @@ class LanguageModel(object):
             rnn_t = rnn_output[t]
             y_t = tf.reshape(self.output_seq[:, t], shape=(self.batch_size, 1))
             step_loss = tf.nn.sampled_softmax_loss(weights=self.w_proj, biases=self.b_proj, inputs=rnn_t,
-                                                   labels=y_t, num_sampled=25, num_classes=self.vocab_size)
+                                                   labels=y_t, num_sampled=100, num_classes=self.vocab_size)
             losses.append(step_loss)
             outputs.append(tf.matmul(rnn_t, tf.transpose(self.w_proj)) + self.b_proj)
         self.step_losses = losses
         self.output = outputs
+        self.softmax = tf.nn.softmax(self.output)
         self.loss = tf.reduce_mean(self.step_losses)
     def compile(self,lr=1e-3):
         self.loss_function = tf.reduce_mean(self.loss)
@@ -43,4 +46,25 @@ class LanguageModel(object):
         #self.opt.run(session=self.sess,feed_dict={self.input_seq: X_batch, self.output_seq: Y_batch})
         _, loss_value = self.sess.run([self.opt, self.loss],feed_dict={self.input_seq: X_batch, self.output_seq: Y_batch})
         return loss_value
+    def predict(self,X,asarray=True):
+        preds = self.sess.run(self.softmax, feed_dict={self.input_seq: X})
+        if asarray:
+            preds = np.asarray(preds)
+            ## Make dimensions more sensible ##
+            preds = np.swapaxes(preds,0,1)
+        return preds
+    def evaluate(self,X,Y):
+        preds = self.sess.run(self.softmax, feed_dict={self.input_seq: X})
+        preds = np.asarray(preds)
+        preds = np.swapaxes(preds, 0, 1)
+        log_prob = 0.
+        ## Note we're only going to use the non-zero entries ##
+        for i in range(len(X)):
+            for j in range(len(X[i])):
+                if X[i,j] != 0:
+                    correct_prob = preds[i,j,Y[i,j]]
+                    log_prob += np.log(correct_prob)
+        log_prob /= np.float(Y.shape[0] * Y.shape[1])
+        perplexity = np.exp(-log_prob)
+        return perplexity
 
--- train.py
+++ train.py
@@ -2,6 +2,10 @@ from data_utils import Dataset
 from keras.utils import generic_utils
 from model import LanguageModel
 import numpy as np
+import tensorflow as tf
+import time as time
+
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 data_dir = '/mnt/raid1/billion-word-corpus/1-billion-word-language-modeling-benchmark/training-monolingual.tokenized.shuffled/'
 NUM_WORDS = 125000
@@ -10,16 +14,17 @@ seq_len = 25
 dataset = Dataset(data_dir,NUM_WORDS)
 X,Y = dataset.create_X_Y(seq_len=seq_len,one_hot_y=False)
 
-batch_size = 32
+batch_size = 256
 embed_size = 256
-num_epochs = 2
+num_epochs = 10
 
 params = {}
 params['vocab_size'] = dataset.vocab_size
 params['num_classes'] = dataset.vocab_size
 params['batch_size'] = batch_size
 params['seq_len'] = seq_len
-params['hidden_dim'] = 128
+params['hidden_dim'] = 512
+params['num_layers'] = 2
 
 model = LanguageModel(params)
 model.compile()
@@ -28,10 +33,16 @@ progbar = generic_utils.Progbar(len(X))
 for epoch in range(num_epochs):
     batches = range(0,len(X)/batch_size)
     for batch in batches[:-1]:
+        t0 = time.time()
         start = batch*batch_size
         end = (batch+1)*batch_size
         X_batch = X[start:end]
         Y_batch = Y[start:end]
+        #perp =  model.evaluate(X_batch,Y_batch)
         loss = model.train_on_batch(X_batch,Y_batch)
         perp = np.exp(np.float32(loss))
-        progbar.add(len(X_batch), values=[("train loss", loss),("train perplexity", perp)])
+        t1 = time.time()
+        wps = np.round((batch_size * seq_len)/(t1-t0))
+        progbar.add(len(X_batch), values=[("loss", loss),("perplexity", perp),("words/sec", wps)])
+
+
