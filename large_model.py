0ca0241e38197b2cd5672c4edbbed11aad586366
beamandrew <beam.andrew@gmail.com>
2016-12-22 16:48:24 -0500

    serialized tokenizer

--- large_model.py
+++ large_model.py
@@ -5,9 +5,6 @@ from keras.optimizers import Adam,SGD
 import keras.backend as K
 import numpy as np
 
-```
-Builds a large language model spread across multiple GPUs
-```
 
 class LargeLanguageModel(object):
     def __init__(self,params):
